e49128b3be150c5e76602d234d168cae6e3dd75c
victor_praise <vpraise27@gmail.com>
2021-08-13 12:01:02 +0100

    functionality done

--- src/components/spotify.js
+++ src/components/spotify.js
@@ -1,5 +1,5 @@
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'https://spotify-clone-67e8e3.netlify.app/';
 const cliendId = '98b937b360c64655a50c0f447339b7ac';
 const scopes = [
     "user-read-currently-playing",
